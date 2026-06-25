a51a86cb795a2a8601952bef0b72f7f897c7a768
Visual Code <visualcodejs@gmail.com>
2026-06-25 14:01:52 +0530

    fix responsiveness of headbar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,11 +10,11 @@ const Home = () => {
   return (
     <div className="w-11/12 mx-auto">
       {/* section1 -> instructor btn, heading, para, authBtn */}
-      <div className="pt-20 w-[50%] mx-auto">
-        <div className="flex flex-col gap-9">
+      <div className="pt-4 sm:pt-20 w-full mx-auto">
+        <div className="flex flex-col gap-4">
           {/* instructor btn */}
           <div className="w-full">
-            <div className="flex justify-center">
+            <div className="flex justify-start sm:justify-center">
               <div className="border border-[#0d1118] rounded-[100px] py-2 px-4.5 flex items-center gap-2.5 bg-[#161D29] text-[#999DAA] font-medium hover:scale-95 transition-all duration-200 shadow-sm shadow-[#40464f]">
                 <p>Become an Instructor</p>
                 <FaArrowRight />
@@ -23,20 +23,19 @@ const Home = () => {
           </div>
 
           {/* heading */}
-          <div className="w-full flex justify-center">
-            <div className="text-[#F1F2FF] font-semibold text-4xl leading-11 tracking-[-2%] flex items-center gap-2">
-              <p>Empower Your Future with</p>
-              <HighlightedText text="Coding School" />
+          <div className="w-full flex justify-start sm:justify-center mt-4">
+            <div className="text-[#F1F2FF] font-semibold text-3xl sm:text-4xl leading-7 sm:leading-11 tracking-[-2%]">
+              <p>Empower Your Future with <HighlightedText text="Coding School" /> </p>
             </div>  
           </div> 
           
           {/* para */}
           <div className="w-full">
-            <p className="text-[#838894] text-center font-medium">With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors.</p>
+            <p className="text-[#838894] text-left sm:text-center font-medium">With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors.</p>
           </div>
 
           {/* auth btns */}
-          <div className="w-full flex items-center justify-center gap-6">
+          <div className="w-full flex items-center justify-start sm:justify-center gap-6">
             <HighlightedBtn btnText="Learn More" path="/signup" isPrimaryBtn={true} />
             <HighlightedBtn btnText="Book a Demo" path="/login" isPrimaryBtn={false} />
           </div>
